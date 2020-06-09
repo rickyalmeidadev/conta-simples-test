cbacd15f312bdaf1fa88261c7650973977b4f41c
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-06-09 16:54:27 -0300

    add form for login, no logic

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const Login = () => <h1>Login</h1>;
+const Login = () => (
+  <form>
+    <fieldset>
+      <legend>Entrar</legend>
+
+      <label htmlFor="cpf">
+        CPF
+        <input type="text" id="cpf" name="cpf" data-testid="cpf" />
+      </label>
+
+      <label htmlFor="email">
+        E-mail
+        <input type="email" id="email" name="email" data-testid="email" />
+      </label>
+
+      <label htmlFor="password">
+        Senha
+        <input
+          type="password"
+          id="password"
+          name="password"
+          data-testid="password"
+        />
+      </label>
+
+      <button type="submit" data-testid="submit">
+        Entrar
+      </button>
+    </fieldset>
+  </form>
+);
 
 export default Login;
