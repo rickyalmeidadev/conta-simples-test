51c3dee6add391ac02f853581548eebc494c4a3d
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-06-11 00:42:34 -0300

    add link to account card

--- src/__tests__/AccountCard.spec.js
+++ src/__tests__/AccountCard.spec.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, cleanup } from '@testing-library/react';
 import AccountCard from '../components/AccountCard';
 
 afterEach(cleanup);
 
 describe('<AccountCard />', () => {
+  const mockCard = {
+    title: 'Conta corrente',
+    description: 'Consulte entradas e saídas pelo seu estrato.',
+    buttonText: 'Ver estrato',
+    link: '/statement',
+    test: 'statement',
+  };
+
   it('renders correctly', () => {
-    const { asFragment } = render(<AccountCard />);
+    const { asFragment } = render(
+      <Router>
+        <AccountCard {...mockCard} />
+      </Router>,
+    );
+
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('displays the cards', () => {
-    const mockCard = {
-      title: 'Conta corrente',
-      description: 'Consulte entradas e saídas pelo seu estrato.',
-      button: 'Ver estrato',
-      test: 'statement',
-    };
-    const { getByTestId } = render(<AccountCard {...mockCard} />);
+    const { getByTestId, getByText } = render(
+      <Router>
+        <AccountCard {...mockCard} />
+      </Router>,
+    );
 
-    expect(getByTestId('statement')).toBeDefined();
+    expect(getByTestId(mockCard.test)).toBeDefined();
+    expect(getByText(mockCard.title)).toBeDefined();
+    expect(getByText(mockCard.description)).toBeDefined();
+    expect(getByText(mockCard.buttonText)).toBeDefined();
   });
 });
--- src/components/AccountCard/index.jsx
+++ src/components/AccountCard/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const AccountCard = ({ title, description, button, test }) => (
+const AccountCard = ({ title, description, buttonText, link, test }) => (
   <article data-testid={test}>
     <h2>{title}</h2>
     <p>{description}</p>
-    <button type="button">{button}</button>
+    <Link to={link}>{buttonText}</Link>
   </article>
 );
 
